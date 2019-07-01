a9d8d13187a3cbb64d2566199efe8ecabe2215d4
idleherb <idleherb@posteo.org>
2019-07-01 07:19:01 +0200

    Add CircleCI integration

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot game-of-life-clj.core
+  :min-lein-version "2.0.0"
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[midje "1.9.7" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-cloverage "1.1.1"]
