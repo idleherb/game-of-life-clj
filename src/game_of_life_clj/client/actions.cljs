64737d0887f3454117d72daaa87fa1bbf7271962
idleherb <idleherb@posteo.org>
2019-07-02 13:14:59 +0200

    Housekeeping

--- src/game_of_life_clj/client/actions.cljs
+++ src/game_of_life_clj/client/actions.cljs
@@ -5,17 +5,17 @@
             [game-of-life-clj.domain.grid :as g]
             [game-of-life-clj.domain.util :as u]))
 
+(defn- reset-game!
+  []
+  (when-let [exit-ch (get-in @s/state [:app :exit-ch])]
+    (a/go (a/>! exit-ch true)))
+  (let [new-exit-ch (a/chan)]
+    (swap! s/state assoc-in [:app :exit-ch] new-exit-ch)
+    new-exit-ch))
+
 (defn- dead?
   [grid]
   (every? nil? (:v grid)))
-
-(defn- reset-game!
-  []
-  (let [new-exit-ch (a/chan)]
-    (when-let [exit-ch (get-in @s/state [:app :exit-ch])]
-      (a/go (a/>! exit-ch true)))
-    (swap! s/state assoc-in [:app :exit-ch] new-exit-ch)
-    new-exit-ch))
 
 (defn run-game!
   [width height rule]
@@ -27,10 +27,10 @@
       (a/alt!
         exit-ch nil
         (a/timeout 5) (cond
-          (dead? grid)       (println (str "Dead in generation "   gen "."))
-          (= grid prev-grid) (println (str "Stable in generation " gen "."))
-          :else (do
-                  (swap! s/state assoc :grid grid)
-                  (recur (g/step grid rule)
-                         grid
-                         (inc gen))))))))
+                        (dead? grid)       (println (str "Dead in generation "   gen "."))
+                        (= grid prev-grid) (println (str "Stable in generation " gen "."))
+                        :else (do
+                                (swap! s/state assoc :grid grid)
+                                (recur (g/step grid rule)
+                                       grid
+                                       (inc gen))))))))
